14cdae55a4e2c359b137d45e946f6a45acebc04a
Evgeny Pashkin <epashkin@cognitive.ru>
2014-05-15 15:04:12 +0400

    World is 2D now

--- src/GameLogic/Data/World.hs
+++ src/GameLogic/Data/World.hs
@@ -6,13 +6,13 @@ import System.Random
 import Util.Shuffle
 
 
-type WorldPos = Int
-startWorlPos = 1
+type WorldPos = (Int, Int)
+startWorldPos = (1,1)
 
 type World = Array WorldPos Cell
 
 mkEmptyWorld :: Int -> World
-mkEmptyWorld size = array (startWorlPos, size) [(i, mkCell 0 0) | i <- [1..size]]
+mkEmptyWorld size = array (startWorldPos, (size, size)) [(pos, mkCell 0 0) | x <- [1..size], y <- [1..size], let pos = (x,y)]
 
 mkStartWorld :: Int ->Int -> StdGen -> (World, StdGen)
 mkStartWorld size numPlayers gen = placeWorldPlayers (mkEmptyWorld size) numPlayers gen
@@ -20,11 +20,36 @@ mkStartWorld size numPlayers gen = placeWorldPlayers (mkEmptyWorld size) numPlay
 setWorldCell :: World -> WorldPos -> Cell -> World
 setWorldCell world pos cell = world // [(pos, cell)]
 
+getWorldSize :: World -> Int
+getWorldSize world = 
+    let (startWorldPos, (size, _)) = bounds world
+    in size
+
 placeWorldPlayers :: World -> Int -> StdGen -> (World, StdGen)
 placeWorldPlayers world numPlayers gen =
     let players = [1..numPlayers]
-	positions = [pl * 2 | pl <- players]
+	positions = [calcStartPos world numPlayers pl | pl <- players]
         (positions', gen') = shuffle gen positions
         playersPosition = zipWith (\a b -> (a,b)) positions' players
         world' = foldl (\w (pos, pl) -> setWorldCell w pos (mkCell 1 pl)) world playersPosition :: World
     in (world', gen')
+
+calcStartPos :: World -> Int -> Int -> WorldPos
+calcStartPos world numPlayers num = 
+    let list = playersStartPosXList (getWorldSize world) numPlayers 
+        cols = playersStartPosCols numPlayers
+        xInd = ((num-1) `mod` cols)
+        yInd = ((num-1) `div` cols)
+    in (list !! xInd, list !! yInd)
+
+-- return number of columns of players start positions by number of players
+playersStartPosCols :: Int -> Int
+playersStartPosCols 4 = 2
+playersStartPosCols 16 = 4
+
+-- return list x-coords start positions
+playersStartPosXList :: Int -> Int -> [Int]
+playersStartPosXList size numPlayers =
+    let cols = playersStartPosCols numPlayers
+        dist = size `div` cols
+    in map (\i -> (i-1)*dist + (dist `div` 2) - 1) [1..cols]
--- src/Main.hs
+++ src/Main.hs
@@ -9,5 +9,11 @@ main = do
    gen1 <- newStdGen
 --   let gen1 = mkStdGen 100
    let (w2, gen2) = mkStartWorld 8 4 gen1
-   print $ show w2
+--   print $ show w2
+   let players = [1..4]
+       positions = [calcStartPos w2 (length players) pl | pl <- players]
+       xList = playersStartPosXList (getWorldSize w2) (length players)
+   print $ show positions
+   print $ show xList
+
 --   boot
