eaba0057d3864a32a7005966117d4cd6df797a2e
Evgeny Pashkin <epashkin@cognitive.ru>
2014-05-17 10:32:55 +0400

    Moved runStartupTest to Boot.hs

--- src/Application/Boot.hs
+++ src/Application/Boot.hs
@@ -3,7 +3,22 @@ module Application.Boot where
 import Debug.Trace
 import GameLogic.Data.Facade
 import Application.Game.Engine
+import System.Random
 
 boot = do
-           game <- newGame
-           trace "Starting" $ runEngine game
+  runStartupTest
+  game <- newGame
+  trace "Starting" $ runEngine game
+
+runStartupTest = do
+   gen1 <- newStdGen
+--   let gen1 = mkStdGen 100
+   g1 <- newGame
+   traceIO $ show g1
+   let players = [1..defNumPlayers]
+       w2 = getWorld g1
+       positions = [calcStartPos w2 (length players) pl | pl <- players]
+       xList = playersStartPosXList defWorldSize defNumPlayers
+   traceIO $ show positions
+   traceIO $ show xList
+   traceIO $ show $ getGameCell g1 (3,3)
--- src/Main.hs
+++ src/Main.hs
@@ -2,23 +2,4 @@ module Main where
 
 import Application.Boot
 
-import GameLogic.Data.Facade
-import System.Random
-import Debug.Trace
-
-main = do
-   runStartupTest
-   boot
-
-runStartupTest = do
-   gen1 <- newStdGen
---   let gen1 = mkStdGen 100
-   g1 <- newGame
-   traceIO $ show g1
-   let players = [1..defNumPlayers]
-       w2 = getWorld g1
-       positions = [calcStartPos w2 (length players) pl | pl <- players]
-       xList = playersStartPosXList defWorldSize defNumPlayers
-   traceIO $ show positions
-   traceIO $ show xList
-   traceIO $ show $ getGameCell g1 (3,3)
+main = boot
