038b7890b7dd227bf3099b695d53d1001a252d7e
Evgeny Pashkin <epashkin@cognitive.ru>
2014-05-26 19:49:19 +0400

    Extracted drawPanel to View.Panel module

---
+++ src/View/Panel.hs
@@ -0,0 +1,63 @@
+module View.Panel where
+
+import Control.Lens
+import View.State
+import View.Convert
+import GameLogic.Data.Facade
+import Middleware.Gloss.Facade
+
+--TODO: Show position
+--TODO: Show minimap
+drawPanel :: State -> Picture
+drawPanel state =
+    let size = state ^. windowSize
+        width = fromIntegral $ fst size
+        height = fromIntegral $ snd size
+        halfWidth = width / 2
+        halfHeight = height / 2
+        shiftX = halfWidth - (panelWidth/2)
+        rect = Color panelBkColor $ rectangleSolid panelWidth height
+        playerPicts = mapP drawPlayer $ state ^. game . players
+        playersPict = Translate 0 (halfHeight - 30) $ Pictures playerPicts
+    in Translate shiftX 0 $ Pictures [rect, playersPict]
+
+drawPlayer :: (Int, Player) -> Picture
+drawPlayer (index, player)
+    = Translate 0 shiftY . Color color . Pictures $ drawPlayerInfoParts player
+    where color = playerColor index
+          shiftY = (0.5 - fromIntegral index) * playerInfoHeight
+
+drawPlayerInfoParts :: Player -> [Picture]
+drawPlayerInfoParts player = fmap p playerInfoParts
+    where p (shift, p) = Translate (playerInfoWidth*shift) 0 . drawInfoText $ p player
+
+playerInfoParts :: [(Float, Player -> String)]
+playerInfoParts = [(-0.50, show.view num)
+                  ,(-0.23, show.view free)
+                  ,(-0.01, show.view remain)
+                  ,( 0.22, shieldText)
+                  ,( 0.40, aggrText)
+                  ]
+
+drawInfoText :: String -> Picture
+drawInfoText = Translate 0 (-playerInfoHeight/2)
+               . Scale panelTextScale panelTextScale . Text
+
+shieldText :: Player -> String
+shieldText player
+    | strength < 128
+    = show strength
+    | active
+    = "+1"
+    | otherwise 
+    = "+0"
+    where strength = player ^. shieldStrength
+          active = player ^. shieldActive
+
+aggrText :: Player -> String
+aggrText player
+    | aggro > 0
+    = show aggro
+    | otherwise 
+    = ""
+    where aggro = player ^. aggr
--- src/View/View.hs
+++ src/View/View.hs
@@ -2,6 +2,7 @@ module View.View where
 
 import Control.Lens
 import View.State
+import View.Panel
 import View.Convert
 import GameLogic.Data.Facade
 import Middleware.Gloss.Facade
@@ -51,60 +52,6 @@ drawSelectedCellBox pos color =
         radius = diametr / 2
         delta = radius / 4
 
---TODO: move to ViewPanel.hs
-drawPanel :: State -> Picture
-drawPanel state =
-    let size = state ^. windowSize
-        width = fromIntegral $ fst size
-        height = fromIntegral $ snd size
-        halfWidth = width / 2
-        halfHeight = height / 2
-        shiftX = halfWidth - (panelWidth/2)
-        rect = Color panelBkColor $ rectangleSolid panelWidth height
-        playerPicts = mapP drawPlayer $ state ^. game . players
-        playersPict = Translate 0 (halfHeight - 30) $ Pictures playerPicts
-    in Translate shiftX 0 $ Pictures [rect, playersPict]
-
-drawPlayer :: (Int, Player) -> Picture
-drawPlayer (index, player)
-    = Translate 0 shiftY . Color color . Pictures $ drawPlayerInfoParts player
-    where color = playerColor index
-          shiftY = (0.5 - fromIntegral index) * playerInfoHeight
-
-drawPlayerInfoParts :: Player -> [Picture]
-drawPlayerInfoParts player = fmap p playerInfoParts
-    where p (shift, p) = Translate (playerInfoWidth*shift) 0 . drawInfoText $ p player
-
-playerInfoParts :: [(Float, Player -> String)]
-playerInfoParts = [(-0.50, show.view num)
-                  ,(-0.23, show.view free)
-                  ,(-0.01, show.view remain)
-                  ,( 0.22, shieldText)
-                  ,( 0.40, aggrText)
-                  ]
-
-drawInfoText :: String -> Picture
-drawInfoText = Translate 0 (-playerInfoHeight/2)
-               . Scale panelTextScale panelTextScale . Text
-
-shieldText :: Player -> String
-shieldText player
-    | strength < 128
-    = show strength
-    | active
-    = "+1"
-    | otherwise 
-    = "+0"
-    where strength = player ^. shieldStrength
-          active = player ^. shieldActive
-
-aggrText :: Player -> String
-aggrText player
-    | aggro > 0
-    = show aggro
-    | otherwise 
-    = ""
-    where aggro = player ^. aggr
     
 translateCell :: WorldPos -> Picture -> Picture
 translateCell (x,y) pict =
